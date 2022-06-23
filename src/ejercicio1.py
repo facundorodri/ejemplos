0cf851d7386c770f8d8050148c1d282798318da4
Facu Rodriguez <106170788+facundorodri@users.noreply.github.com>
2022-06-23 16:08:04 -0300

    Ejercicio 1 del TP Python

---
+++ src/ejercicio1.py
@@ -0,0 +1,13 @@
+################
+# Nombre - @facundorodri
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+1. Conversión de temperaturas
+Se quiere transformar temperaturas en grados fahrenheit a grados centígrados y viceversa.
+
+Escribir las funciones para convertir la temperatura en grados centigrados en fahrenheit como un número decimal, utilice esta formula para calcular
+los grados centígrados y retorne el resultado obtenido. Y viceversa.
+
+"""
